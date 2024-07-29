ec3bf1ce9c863d19d14a3716f0f1395ac050568a
leandro Santana <leandro.edv@hotmail.com>
2024-07-29 18:53:55 -0300

    estudos desenvolvimento_JavaScript

---
+++ desenvolvimento_JavaScript/script1.js
@@ -0,0 +1,4 @@
+
+let nome = "leandro";
+let idade = 31;
+console.log("meu nome é: "+ nome + " minha idade é: " + idade)
---
+++ desenvolvimento_JavaScript/script2.js
@@ -0,0 +1,5 @@
+// exemplo de imput para uso com o node js
+
+let rlSynnc = require('readline-sync')
+let nome = rlSynnc.question("qual o seu nome?\n")
+console.log(`olá ${nome}`);
