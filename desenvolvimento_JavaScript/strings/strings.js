ca269ec038ad1fe1e4befb1376db9c4841eb7d94
leandro Santana <leandro.stn.dev@gmail.com>
2024-08-30 16:15:42 -0300

    estudos strings no javaScript

---
+++ desenvolvimento_JavaScript/strings/strings.js
@@ -0,0 +1,36 @@
+var nome = "leandro Santana"
+var x = nome.length;
+
+console.log(x);
+console.log(nome.toUpperCase());
+
+// indice de string
+
+var jogo = "Barcelona vs Real, será um grande jogo!";
+var posicao = jogo.indexOf("Real");
+console.log("posição da palavra Real: " + posicao);
+
+// fatiamento de String
+
+console.log(jogo.slice(0,9));
+console.log(jogo.slice(jogo.indexOf("Real"),-1));
+
+// procurando strings 
+
+console.log(jogo.includes("Madri"));
+
+// concatenando strings
+
+var str1 = "Hello ";
+var str2 = "world!";
+console.log(str1.concat(str2));
+
+//Retirar os espaços no inicio e no fim
+var frase = "     Olá, você está aprendendo javaScript   ";
+console.log(frase.trim());
+
+// Criar um novo Array
+
+var num = "1,2,3,4,5,6";
+var arr = num.split(","); // Utiliza a vísgula para identificar o novo elemento do array
+console.log(arr);
